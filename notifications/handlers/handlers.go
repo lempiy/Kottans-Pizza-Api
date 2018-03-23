6b809c6c3cf3d3328a35579b01f7f84641b3f42d
Anton Lempiy <lempiyada@gmail.com>
2018-03-23 18:23:19 +0200

    Init notifications service

---
+++ notifications/handlers/handlers.go
@@ -0,0 +1,10 @@
+package handlers
+
+import (
+	"github.com/labstack/echo"
+)
+
+//Run - inits and fills app router with handlers.
+func Run(r *echo.Router) {
+	//**//
+}
--- notifications/notifications.go
+++ notifications/notifications.go
@@ -1,9 +1,18 @@
 package main
 
 import (
-    "fmt"
+    "github.com/labstack/echo"
+    "github.com/labstack/echo/middleware"
+    "github.com/lempiy/Kottans-Pizza-Api/notifications/handlers"
+    "os"
 )
 
 func main() {
-    fmt.Println("UNIMPLEMENTED")
+    e := echo.New()
+    e.Use(middleware.Logger())
+    e.Use(middleware.Recover())
+    PORT := os.Getenv("PORT")
+    r := e.Router()
+    handlers.Run(r)
+    e.Logger.Fatal(e.Start(":" + PORT))
 }
